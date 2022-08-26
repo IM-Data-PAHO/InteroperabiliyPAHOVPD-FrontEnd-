9eadc878b9ae5d324d455b85abfcb953895d9bf7
Laura Girleza Martinez Sandoval <mart800976@siigo.com>
2022-08-26 09:42:07 -0500

    Merge branch 'dev' of https://github.com/HISPColombia/InteroperabiliyPAHOVPD-FrontEnd into dev

--- src/app/shared/services/connection.ts
+++ src/app/shared/services/connection.ts
@@ -1,5 +1,5 @@
 export class Connection{
 
-   //public static ENDPOINTBACK="http://localhost:9929/apipaho/";
-   public static ENDPOINTBACK="http://20.206.101.82/apipaho/";
+   public static ENDPOINTBACK="http://localhost:9929/apipaho/";
+   //public static ENDPOINTBACK="http://20.206.101.82/apipaho/";
 }
