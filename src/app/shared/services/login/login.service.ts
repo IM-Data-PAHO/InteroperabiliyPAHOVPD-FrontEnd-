246b79f0e82732efb58355a2bf2eebb92bdfa861
Laura Girleza Martinez Sandoval <mart800976@siigo.com>
2022-09-09 19:37:40 -0500

    Cambios de documentación

--- src/app/shared/services/login/login.service.ts
+++ src/app/shared/services/login/login.service.ts
@@ -20,32 +20,54 @@ export class LoginService {
     this._cookieService.deleteAll();
     return this._http.get<any[]>('./assets/data/usuarios.json');
   }
+  /*
+  *Función que llama al endpoint al back que hace el login
+  */
   login(user: login): Observable<LoginResponse> {
     this._cookieService.deleteAll();
     var response =  this._http.post<LoginResponse>(Connection.ENDPOINTBACK + 'Login/getUser', user);
     return response; 
   }
+  /*
+  *Función que llama al endpoint al back que trae las configuraciones del usuario
+  */
   getlanguage(token: string): Observable<userSettings> {
     var response = this._http.get<userSettings>(Connection.ENDPOINTBACK + 'Login/getUserSetting/' + token);
     return response;   
   } 
 
+  /*
+  *Función que asigna a las cookies el token, usuario y rol
+  */
   setToken(token: string, user: string){
     this._cookieService.set("token", token);
     this._cookieService.set("usuario", user);
     this._cookieService.set("rol", user);
   }
-
+/*
+  *Función que obtiene el token
+  */
   getToken(){
     return this._cookieService.get("token");
   }
+
+  /*
+  *Función que obtiene el usuario
+  */
   getUsuario(){
     return this._cookieService.get("usuario");
   }
+
+  /*
+  *Función que obtiene el rol 
+  */
   getRole(){
   return this._cookieService.get("rol");
   }
-
+  
+  /*
+  *Función para cerrar la sesión del usuario
+  */
   logout(){
     this._cookieService.deleteAll();
     this._router.navigateByUrl('login');
--- src/app/shared/services/uploader/uploader.service.ts
+++ src/app/shared/services/uploader/uploader.service.ts
@@ -13,23 +13,35 @@ export class UploaderService {
 
   constructor(private _http : HttpClient, private _loginService : LoginService) { }
 
+  /*
+  *Función que llama al endpoint al back para obtener todos los programas mapeados
+  */
   getSelectProgram() : Observable<Programs>{
     let token = this._loginService.getToken();
     return this._http.get<Programs>(Connection.ENDPOINTBACK + 'DhisIntegration/getAllProgram/' + token);
   }
 
-
+  /*
+  *Función que llama al endpoint al back para ejecutar la importación
+  */
   uploadFile(param: any) : Observable<any>{
     let url = this._http.post<any>(Connection.ENDPOINTBACK + 'DataImport', param);
     return url;
       
   }
+
+  /*
+  *Función que llama al endpoint al back para ejecutar la pre-validación
+  */
   preuploadFile(param: any) : Observable<any>{
     console.log("Post: startDryRun")
     return this._http.post<any>(Connection.ENDPOINTBACK + 'DhisIntegration/startDryRun', param);
       
   }
-
+  
+  /*
+  *Función que llama al endpoint al back para traer todo el historial relacionado a las importaciones
+  */
   getHistoryUser(user: string, token: string) : Observable<History[]>{
   
     //console.log("URL: "+Connection.ENDPOINTBACK + 'history/user?user='+user+'&token='+token)
